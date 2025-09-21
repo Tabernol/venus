f6be50d81e1ebb0d27e62f1cedc877e62033964c
Tabernol <madventurerace@gmail.com>
2025-09-21 14:50:14 +0300

    added title for gallery

--- src/pages/LandingPages/Gallery/index.js
+++ src/pages/LandingPages/Gallery/index.js
@@ -10,9 +10,11 @@ import MKButton from "../../../components/MKButton";
 import DefaultFooter from "../../../examples/Footers/DefaultFooter";
 import footerRoutes from "../../../footer.routes";
 import heroMp4 from "assets/video/golden_kiss_moment_simple.mp4";
+import { useTranslation } from "react-i18next";
 
 
 export default function Gallery() {
+    const { t } = useTranslation();
     return (
         <>
             <DefaultNavbar
@@ -34,79 +36,76 @@ export default function Gallery() {
             >
                 <div>
                     <MKTypography variant="h2" color="white" fontWeight="bold" mb={2}>
-                        Створимо ваш ідеальний весільний день
-                    </MKTypography>
-                    <MKTypography variant="body1" color="white" opacity={0.85} mb={3}>
-                        Організація, декор, координація — все під ключ.
+                        {t('gall.title')}
                     </MKTypography>
                 </div>
             </HeroVideoBackground>
-            {/*<MKBox*/}
-            {/*    minHeight="75vh"*/}
-            {/*    width="100%"*/}
-            {/*    sx={{*/}
-            {/*        backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>*/}
-            {/*            `${linearGradient(*/}
-            {/*                rgba(gradients.dark.main, 0.6),*/}
-            {/*                rgba(gradients.dark.state, 0.6)*/}
-            {/*            )}, url(${bgImage})`,*/}
-            {/*        backgroundSize: "cover",*/}
-            {/*        backgroundPosition: "center",*/}
-            {/*        display: "grid",*/}
-            {/*        placeItems: "center",*/}
-            {/*    }}*/}
-            {/*>*/}
-                {/*<Container>*/}
-                {/*    <Grid*/}
-                {/*        container*/}
-                {/*        item*/}
-                {/*        xs={12}*/}
-                {/*        lg={8}*/}
-                {/*        justifyContent="center"*/}
-                {/*        alignItems="center"*/}
-                {/*        flexDirection="column"*/}
-                {/*        sx={{ mx: "auto", textAlign: "center" }}*/}
-                {/*    >*/}
-                {/*        <MKTypography*/}
-                {/*            variant="h1"*/}
-                {/*            color="white"*/}
-                {/*            sx={({ breakpoints, typography: { size } }) => ({*/}
-                {/*                [breakpoints.down("md")]: {*/}
-                {/*                    fontSize: size["3xl"],*/}
-                {/*                },*/}
-                {/*            })}*/}
-                {/*        >*/}
-                {/*            Work with an amazing design*/}
-                {/*        </MKTypography>*/}
-                {/*        <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>*/}
-                {/*            We&apos;re constantly trying to express ourselves and actualize our dreams. If you*/}
-                {/*            have the opportunity to play this game*/}
-                {/*        </MKTypography>*/}
-                {/*        <MKButton color="default" sx={{ color: ({ palette: { dark } }) => dark.main }}>*/}
-                {/*            create account*/}
-                {/*        </MKButton>*/}
-                {/*        <MKTypography variant="h6" color="white" mt={8} mb={1}>*/}
-                {/*            Find us on*/}
-                {/*        </MKTypography>*/}
-                {/*        <MKBox display="flex" justifyContent="center" alignItems="center">*/}
-                {/*            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>*/}
-                {/*                <i className="fab fa-facebook" />*/}
-                {/*            </MKTypography>*/}
-                {/*            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>*/}
-                {/*                <i className="fab fa-instagram" />*/}
-                {/*            </MKTypography>*/}
-                {/*            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>*/}
-                {/*                <i className="fab fa-twitter" />*/}
-                {/*            </MKTypography>*/}
-                {/*            <MKTypography component="a" variant="body1" color="white" href="#">*/}
-                {/*                <i className="fab fa-google-plus" />*/}
-                {/*            </MKTypography>*/}
-                {/*        </MKBox>*/}
-                {/*    </Grid>*/}
-                {/*</Container>*/}
+            <MKBox
+                minHeight="75vh"
+                width="100%"
+                sx={{
+                    backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+                        `${linearGradient(
+                            rgba(gradients.dark.main, 0.6),
+                            rgba(gradients.dark.state, 0.6)
+                        )}, url(${bgImage})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    display: "grid",
+                    placeItems: "center",
+                }}
+            >
+                <Container>
+                    <Grid
+                        container
+                        item
+                        xs={12}
+                        lg={8}
+                        justifyContent="center"
+                        alignItems="center"
+                        flexDirection="column"
+                        sx={{ mx: "auto", textAlign: "center" }}
+                    >
+                        <MKTypography
+                            variant="h1"
+                            color="white"
+                            sx={({ breakpoints, typography: { size } }) => ({
+                                [breakpoints.down("md")]: {
+                                    fontSize: size["3xl"],
+                                },
+                            })}
+                        >
+                            Work with an amazing design
+                        </MKTypography>
+                        <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
+                            We&apos;re constantly trying to express ourselves and actualize our dreams. If you
+                            have the opportunity to play this game
+                        </MKTypography>
+                        <MKButton color="default" sx={{ color: ({ palette: { dark } }) => dark.main }}>
+                            create account
+                        </MKButton>
+                        <MKTypography variant="h6" color="white" mt={8} mb={1}>
+                            Find us on
+                        </MKTypography>
+                        <MKBox display="flex" justifyContent="center" alignItems="center">
+                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+                                <i className="fab fa-facebook" />
+                            </MKTypography>
+                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+                                <i className="fab fa-instagram" />
+                            </MKTypography>
+                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
+                                <i className="fab fa-twitter" />
+                            </MKTypography>
+                            <MKTypography component="a" variant="body1" color="white" href="#">
+                                <i className="fab fa-google-plus" />
+                            </MKTypography>
+                        </MKBox>
+                    </Grid>
+                </Container>
 
 
-            {/*</MKBox>*/}
+            </MKBox>
             <MKBox pt={6} px={1} mt={6}>
                 <DefaultFooter content={footerRoutes} />
             </MKBox>
