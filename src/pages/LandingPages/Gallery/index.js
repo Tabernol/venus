6fcb87f7d64808840d8d513e06ee8b653cd8914a
Tabernol <madventurerace@gmail.com>
2025-10-20 16:55:32 +0300

    added template for gallery

--- src/pages/LandingPages/Gallery/index.js
+++ src/pages/LandingPages/Gallery/index.js
@@ -1,18 +1,16 @@
 import DefaultNavbar from "../../../examples/Navbars/DefaultNavbar";
 import routes from "../../../routes";
 import MKBox from "../../../components/MKBox";
-import bgImage from "../../../assets/images/bg-about-us.jpg";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import MKTypography from "../../../components/MKTypography";
 import HeroVideoBackground from "../../../components/HeroVideoBackground";
-import MKButton from "../../../components/MKButton";
 import DefaultFooter from "../../../examples/Footers/DefaultFooter";
 import footerRoutes from "../../../footer.routes";
-// import heroMp4 from "assets/video/golden_kiss_moment_simple.mp4";
 import heroMp4 from "assets/video/golden_kiss_moment_2.mp4";
 import { useTranslation } from "react-i18next";
 import venusLogo from "../../../assets/images/logo/logo.png";
+
+// Gallery sections
+import CouplesStories from "./sections/CouplesStories";
 
 
 export default function Gallery() {
@@ -33,8 +31,6 @@ export default function Gallery() {
             />
             <HeroVideoBackground
                 srcMp4={heroMp4}
-                // srcWebm={heroWebm}
-                // poster={heroPoster}
                 height="80vh"
             >
                 <div>
@@ -43,71 +39,9 @@ export default function Gallery() {
                     </MKTypography>
                 </div>
             </HeroVideoBackground>
-            <MKBox
-                minHeight="75vh"
-                width="100%"
-                sx={{
-                    backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
-                        `${linearGradient(
-                            rgba(gradients.dark.main, 0.6),
-                            rgba(gradients.dark.state, 0.6)
-                        )}, url(${bgImage})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    display: "grid",
-                    placeItems: "center",
-                }}
-            >
-                <Container>
-                    <Grid
-                        container
-                        item
-                        xs={12}
-                        lg={8}
-                        justifyContent="center"
-                        alignItems="center"
-                        flexDirection="column"
-                        sx={{ mx: "auto", textAlign: "center" }}
-                    >
-                        <MKTypography
-                            variant="h1"
-                            color="white"
-                            sx={({ breakpoints, typography: { size } }) => ({
-                                [breakpoints.down("md")]: {
-                                    fontSize: size["3xl"],
-                                },
-                            })}
-                        >
-                        </MKTypography>
-                        <MKTypography variant="body1" color="white" opacity={0.8} mt={1} mb={3}>
-                            We&apos;re constantly trying to express ourselves and actualize our dreams. If you
-                            have the opportunity to play this game
-                        </MKTypography>
-                        <MKButton color="default" sx={{ color: ({ palette: { dark } }) => dark.main }}>
-                            create account
-                        </MKButton>
-                        <MKTypography variant="h6" color="white" mt={8} mb={1}>
-                            Find us on
-                        </MKTypography>
-                        <MKBox display="flex" justifyContent="center" alignItems="center">
-                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                                <i className="fab fa-facebook" />
-                            </MKTypography>
-                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                                <i className="fab fa-instagram" />
-                            </MKTypography>
-                            <MKTypography component="a" variant="body1" color="white" href="#" mr={3}>
-                                <i className="fab fa-twitter" />
-                            </MKTypography>
-                            <MKTypography component="a" variant="body1" color="white" href="#">
-                                <i className="fab fa-google-plus" />
-                            </MKTypography>
-                        </MKBox>
-                    </Grid>
-                </Container>
 
-
-            </MKBox>
+            {/* Couples Stories Section */}
+            <CouplesStories />
             <MKBox pt={6} px={1} mt={6}>
                 <DefaultFooter content={footerRoutes} />
             </MKBox>
---
+++ src/pages/LandingPages/Gallery/sections/CouplesStories.js
@@ -0,0 +1,116 @@
+/*
+=========================================================
+* Material Kit 2 React - v2.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-kit-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+// Images
+import couple1Image from "assets/images/couples/pair1/couple1.jpg";
+
+function CouplesStories() {
+  // Array of couples data - you can easily add more couples here
+  const couplesData = [
+    {
+      id: 1,
+      image: couple1Image,
+      name: "Emma & James",
+      story: "A beautiful love story that began on a sunny afternoon in Paris. Their journey together has been filled with adventure, laughter, and endless support for one another.",
+    },
+    // Add more couples here as you add more images
+    // {
+    //   id: 2,
+    //   image: couple2Image,
+    //   name: "Sophia & Michael",
+    //   story: "Met at a coffee shop and never looked back. Their shared passion for travel and photography brought them together...",
+    // },
+    // {
+    //   id: 3,
+    //   image: couple3Image,
+    //   name: "Isabella & Alexander",
+    //   story: "A chance encounter at a bookstore turned into the greatest love story. They bond over literature and art...",
+    // },
+  ];
+
+  return (
+    <MKBox component="section" py={6}>
+      <Container>
+        <Grid container item xs={12} lg={10} sx={{ mx: "auto", mb: 6 }}>
+          <MKBox width="100%" textAlign="center">
+            <MKTypography variant="h2" fontWeight="bold" mb={1}>
+              Love Stories
+            </MKTypography>
+            <MKTypography variant="body1" color="text" mb={2}>
+              Discover the beautiful journeys of couples who found their perfect match
+            </MKTypography>
+          </MKBox>
+        </Grid>
+        <Grid container spacing={3}>
+          {couplesData.map((couple) => (
+            <Grid item xs={12} md={6} lg={4} key={couple.id}>
+              <MKBox mb={3}>
+                <Card>
+                  <MKBox position="relative" borderRadius="lg" mx={2} mt={-3}>
+                    <MKBox
+                      component="img"
+                      src={couple.image}
+                      alt={couple.name}
+                      borderRadius="lg"
+                      width="100%"
+                      position="relative"
+                      zIndex={1}
+                    />
+                    <MKBox
+                      borderRadius="lg"
+                      shadow="md"
+                      width="100%"
+                      height="100%"
+                      position="absolute"
+                      left={0}
+                      top={0}
+                      sx={{
+                        backgroundImage: `url(${couple.image})`,
+                        transform: "scale(0.94)",
+                        filter: "blur(12px)",
+                        backgroundSize: "cover",
+                      }}
+                    />
+                  </MKBox>
+                  <MKBox p={3} mt={-1} textAlign="center">
+                    <MKTypography display="inline" variant="h5" textTransform="capitalize" fontWeight="regular">
+                      {couple.name}
+                    </MKTypography>
+                    <MKBox mt={1} mb={2}>
+                      <MKTypography variant="body2" component="p" color="text">
+                        {couple.story}
+                      </MKTypography>
+                    </MKBox>
+                  </MKBox>
+                </Card>
+              </MKBox>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default CouplesStories;
